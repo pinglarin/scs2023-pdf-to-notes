738ac48049b0a222167b3c88db1bdf661e9f3a26
Jidapaminnie <jidapaminnie@gmail.com>
2023-04-11 23:00:31 +0700

    deleted unused files and organized Uploader Page

--- React/src/pages/LandingPages/AboutUs/sections/FileUploader.js
+++ React/src/pages/LandingPages/AboutUs/sections/FileUploader.js
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useModal } from "react-hooks-use-modal";
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+// import MKInput from "components/MKInput";
+import MKButton from "components/MKButton";
+import MKTypography from "components/MKTypography";
+import HorizontalTeamCard from "examples/Cards/TeamCards/HorizontalTeamCard";
 
 function FileUpload() {
   const [selectedFile, setSelectedFile] = useState();
   const [isSelected, setIsSelected] = useState(false);
   // const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const [ModalSuccess, closeSuccess] = useModal("root", {
+    preventScroll: true,
+    closeOnOverlayClick: false,
+  });
+
+  const [ModalFail, closeFail] = useModal("root", {
+    preventScroll: true,
+    closeOnOverlayClick: false,
+  });
+
   const handleChange = (event) => {
     setSelectedFile(event.target.files[0]);
     setIsSelected(true);
   };
 
-  const Uploadvideo = (event) => {
+  const UploadFile = (event) => {
     const formData = new FormData();
     const uploadfile = document.querySelector("#file");
     formData.append("file", uploadfile.files[0]);
@@ -41,26 +64,102 @@ function FileUpload() {
     event.preventDefault();
   };
   return (
-    <form>
-      <h2>Upload a file</h2>
-      <input type="file" id="file" name="file" onChange={handleChange} />
-      {isSelected ? (
-        <div>
-          <h2>File Details</h2>
-          <p>Filename: {selectedFile.name}</p>
-          <p>Filetype: {selectedFile.type}</p>
-          <p>Size in bytes: {selectedFile.size}</p>
-          <p>lastModifiedDate:{selectedFile.lastModifiedDate.toLocaleDateString()}</p>
-        </div>
-      ) : (
-        <p>Select a file to show details</p>
-      )}
-      <div>
-        <button type="submit" onClick={Uploadvideo}>
-          Submit
-        </button>
-      </div>
-    </form>
+    <MKBox component="section" py={12}>
+      <Container>
+        <Grid container item justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
+          <MKTypography variant="h3" mb={1}>
+            Upload PDF a file to generate summary
+          </MKTypography>
+        </Grid>
+        <Grid container item xs={12} lg={7} sx={{ mx: "auto" }}>
+          <MKBox width="100%" component="form" method="post" autocomplete="off">
+            <MKBox p={3}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <MKTypography component="a" fontWeight="regular" color="dark">
+                    <input type="file" id="file" name="file" onChange={handleChange} />
+                    {isSelected ? (
+                      <div>
+                        <Grid
+                          container
+                          item
+                          justifyContent="center"
+                          xs={10}
+                          lg={7}
+                          mx="auto"
+                          textAlign="center"
+                        >
+                          <MKTypography variant="h4" mb={1}>
+                            File Details
+                          </MKTypography>
+                        </Grid>
+                        <p>Filename: {selectedFile.name}</p>
+                        <p>Filetype: {selectedFile.type}</p>
+                        <p>Size in bytes: {selectedFile.size}</p>
+                        <p>
+                          lastModifiedDate: {selectedFile.lastModifiedDate.toLocaleDateString()}
+                        </p>
+                      </div>
+                    ) : (
+                      <p>Select a file to show details</p>
+                    )}
+                  </MKTypography>
+                </Grid>
+              </Grid>
+              <Grid container item justifyContent="center" xs={12} my={2}>
+                <MKButton
+                  onClick={UploadFile}
+                  type="submit"
+                  href="#"
+                  variant="gradient"
+                  color="dark"
+                  fullWidth
+                >
+                  Submit
+                </MKButton>
+              </Grid>
+            </MKBox>
+          </MKBox>
+        </Grid>
+      </Container>
+      <ModalSuccess>
+        <HorizontalTeamCard
+          image="https://c.tenor.com/xVfFIHxAzW4AAAAC/success.gif"
+          name="Notification"
+          position={{ color: "success", label: "Upload Success!" }}
+          description=" "
+        />
+        <MKButton
+          onClick={closeSuccess}
+          type="submit"
+          href="#"
+          variant="gradient"
+          color="dark"
+          fullWidth
+        >
+          Close
+        </MKButton>
+      </ModalSuccess>
+      <ModalFail>
+        <HorizontalTeamCard
+          image="https://c.tenor.com/_EOg1bM-P-AAAAAC/fail.gif"
+          name="Notification"
+          position={{ color: "error", label: "Upload Fail!" }}
+          description="Please try agian"
+        />
+        <MKButton
+          onClick={closeFail}
+          type="submit"
+          href="#"
+          variant="gradient"
+          color="dark"
+          fullWidth
+        >
+          Close
+        </MKButton>
+      </ModalFail>
+    </MKBox>
   );
 }
+
 export default FileUpload;
--- minnieslog.md
+++ minnieslog.md
@@ -1,7 +1,10 @@
 #### Front end stuff
-1. Create a js component to handle file uploads (called FileUploader.js) by using file uploader code https://www.spguides.com/upload-file-in-react-js/ 
+1. Create a js component to handle file uploads (called FileUploader.js) by using file uploader code https://www.spguides.com/upload-file-in-react-js/ and use DragUploader.js as reference
 2. {unused atm} Created DragAndDrop.js to handle drag and drop files instead of clicking a button to upload a file. However, it is hard to integrate the submit function of the normal file upload to the drag and drop one, so this is not used yet, and currently the normal file upload is used.
 3. Create a FastAPI function to handle file upload via http://localhost:8000/gen_summary/
 In main.py (main FastAPI file)  [Create a temporary storage on Server aka FastAPI to store the file while being processed]
 4. Adjust FileUploader.js (temporarily TempUploader.js) to be like DragUploader.js.
-5. **TOBEDONE** >> decorate FileUploader UI
+5. decorate Uploader Page
+
+***TO BE DONE***
+Fill in generator function
